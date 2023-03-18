d08a900a20dec20c769c76fc44b5a0b1c6d52872
Preeti734 <spreeti19@tbc.edu.np>
2023-03-18 22:53:51 +0545

    Variable declaration

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -3,10 +3,21 @@ package com.example.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText cost_of_service;
+    private Button calculate_button;
+    private RadioGroup radiogroup;
+    private Switch round_up_switch;
+    private TextView tip_result;
+    private double tip_amount;
+    private double tip_percent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
