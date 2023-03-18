8b5e08c8684d194503277b9e9fbe89a73f483743
Preeti734 <spreeti19@tbc.edu.np>
2023-03-18 22:55:02 +0545

    Code to make app functional

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -22,6 +23,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+        cost_of_service = findViewById(R.id.cost_of_service);
+        calculate_button = findViewById(R.id.calculate_button);
+        radiogroup = findViewById(R.id.radioGroup);
+        round_up_switch = findViewById(R.id.round_up_switch);
+        tip_result = findViewById(R.id.tip_result);
+
+        calculate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cost = cost_of_service.getText().toString();
+                double costAmount = Double.parseDouble(cost);
+                double checkedRadioButton = radiogroup.getCheckedRadioButtonId();
+                if(checkedRadioButton==R.id.option_twenty_percent){
+                    tip_percent=0.2;
+                } else if (checkedRadioButton==R.id.option_eighteen_percent) {
+                    tip_percent=0.18;
+                }else if (checkedRadioButton==R.id.option_fifteen_percent) {
+                    tip_percent=0.15;
+                }else {
+                    tip_percent=0.0;
+                }
+                tip_amount=costAmount*tip_percent;
+                if (round_up_switch.isChecked()){
+                    tip_amount=Math.round(tip_amount);
+                }
+                tip_result.setText(" "+tip_amount);
+            }
+        });
 
     }
 }
